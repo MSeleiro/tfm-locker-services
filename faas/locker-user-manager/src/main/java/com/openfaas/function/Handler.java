99fd5f7677536b39d200fd95c0124a99d3f21967
MSeleiro <blabla2340918@gmail.com>
2024-04-20 16:11:03 +0000

    Backend & Locker: MariaDB schema and FaaS for CRUD  v0.4.1
    Contains: Function for inserting a test user into the users DB
    
    TODO: Implementation of FaaS functions for CRUD in each table

--- faas/locker-user-manager/src/main/java/com/openfaas/function/Handler.java
+++ faas/locker-user-manager/src/main/java/com/openfaas/function/Handler.java
@@ -2,9 +2,11 @@ package com.openfaas.function;
 
 import java.sql.DriverManager;
 import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.UUID;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
@@ -13,39 +15,59 @@ public class Handler extends com.openfaas.model.AbstractHandler {
 
     private static final String MARIADB_USER = System.getenv("MARIADB_USER");
     private static final String MARIADB_PASS = System.getenv("MARIADB_PASS");
+    private static final String USER_DB = System.getenv("USER_DB");
+
+    public static byte[] UUIDtoByteArray(UUID uuid) {
+        byte[] bytes = new byte[16];
+        ByteBuffer.wrap(bytes)
+                .order(ByteOrder.BIG_ENDIAN)
+                .putLong(uuid.getMostSignificantBits())
+                .putLong(uuid.getLeastSignificantBits());
+        return bytes;
+    }
+
+    public static UUID byteArraytoUUID(byte[] bytes) {
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        long h = bb.getLong();
+        long l = bb.getLong();
+        return new UUID(h, l);
+    }
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
 
-        String s = "jdbc:mariadb://192.168.1.4:3306/mysql?user="+MARIADB_USER+"&password="+MARIADB_PASS;
+        String s = "jdbc:mariadb://192.168.1.4:3306/"+USER_DB+"?user="+MARIADB_USER+"&password="+MARIADB_PASS;
 
         //create connection for a server installed in localhost, with a user "root" with no password
         try (Connection conn = DriverManager.getConnection(s)) {
             // create a Statement
-            try (Statement stmt = conn.createStatement()) {
-                //execute query
-                try (ResultSet rs = stmt.executeQuery("SELECT 'Hello World!'")) {
-                    //position result to first
-                    rs.first();
-                    res.setBody(rs.getString(1)); //result is "Hello World!"
-                }
+            try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (?, ?)")) {
+                User u = new User("Test Test");
+                byte[] uuidBytes = UUIDtoByteArray(u.user_id);
+
+                stmt.setBytes(1, uuidBytes);
+                stmt.setString(2, u.user_name);
+                stmt.executeUpdate();
+                
+                res.setBody("Inverse?: " + (u.user_id.equals(byteArraytoUUID(uuidBytes))));
             }
         } catch(SQLException e) {
-            String b = "";
-            String path = System.getProperty("java.class.path");
-            String[] p;
-            p = path.split(":");
-            for(int i=0; i< p.length; i++) {
-                b += p[i] + "\n";
-            }
             res.setBody(
                 "Error: " + e.getMessage() + 
                 "\n\n" + 
-                "Code and State: " + e.getErrorCode() + " - " + e.getSQLState() + 
-                "\n\n" + 
-                b
-                );
+                "Code and State: " + e.getErrorCode() + " - " + e.getSQLState()
+            );
         }
 	    return res;
+    }
+
+    public class User {
+        public UUID user_id;
+        public String user_name;
+
+        User(String un) {
+            user_id = UUID.randomUUID();
+            user_name = un;
+        }
     }
 }
